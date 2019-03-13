8080c521e511016d0d3fd2ca02e98d2a1f40b532
Dominic Chen <d.c.ddcc@gmail.com>
2019-03-13 19:37:11 -0400

    add script

---
+++ run-threads.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#set -e
+
+if [ $# -ne 1 ]; then
+  echo "$0 <iterations>"
+  exit
+fi
+
+export TM_STATISTICS=1
+
+for i in 1 2 4 8 16 32 64; do
+  for ((j=1; j<=${1}; j++)); do
+    echo "Running $i threads at iteration $j"
+    ./sb7_tt -n ${i} >> sb7.$i.log
+  done
+done
