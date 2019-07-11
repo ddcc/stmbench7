544d8d0c51869705b71ac0d74cade5f500d5c0ee
Dominic Chen <d.c.ddcc@gmail.com>
2019-07-11 03:33:47 -0400

    make run script more robust

--- run-threads.sh
+++ run-threads.sh
@@ -12,6 +12,10 @@ export TM_STATISTICS=1
 for i in 1 2 4 8 16 32 64; do
   for ((j=1; j<=${1}; j++)); do
     echo "Running $i threads at iteration $j"
-    ./sb7_tt -n ${i} >> sb7.$i.log
+    RET=1
+    while [ $RET -ne 0 ]; do
+      ./sb7_tt -n ${i} > sb7.$i.log
+      RET=$?
+    done
   done
 done
